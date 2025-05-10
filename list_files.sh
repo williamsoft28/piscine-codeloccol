b2610bae662bcf8dc61e1f0cfde10f107ee5ca1e
williamsoft28 <wilizbga28@gmail.com>
2025-05-10 22:05:39 +0200

    supresion de 4

--- list_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script pour afficher la liste de tous les fichiers du répertoire courant
-
-echo "Liste des fichiers dans le répertoire courant :"
-ls -lh  # -l : format long, -h : tailles lisibles
