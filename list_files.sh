6043611d90d7f7ec72f9658b0e2ff31c0b57f5aa
williamsoft28 <wilizbga28@gmail.com>
2025-05-10 18:26:08 +0200

    affichage des fichier du repertoire courant

---
+++ list_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script pour afficher la liste de tous les fichiers du répertoire courant
+
+echo "Liste des fichiers dans le répertoire courant :"
+ls -lh  # -l : format long, -h : tailles lisibles
